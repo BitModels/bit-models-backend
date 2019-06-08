6ec5420275f81ad64f4bfff5fbd422ccf5f8553d
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2019-06-08 18:34:34 -0300

    Add admin functions

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@
 const connectToDatabase = require("./db")
 const Area = require("./models/area")
 const Profile = require("./models/profile")
+const AdminLogin = require("./models/adminLogin")
 
 module.exports.areas = async (event, context) => {
   await connectToDatabase()
@@ -87,4 +88,116 @@ module.exports.createProfile = async (event, context) => {
       "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
     },
   }
+}
+
+module.exports.adminLogin = async (event, context) => {
+  try {
+    await connectToDatabase()
+
+  const user = JSON.parse(event.body)
+  const admin = await AdminLogin.find().where({ login: user.login })
+
+  if (admin.length !== 1) {
+    return {
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  }
+
+  const crypto = require('crypto');
+  const hash = crypto.createHmac('sha256', process.env.SECRET)
+                   .update(user.password)
+                   .digest('hex');
+
+  if (admin[0].password === hash) {
+    const jwt  = require('jsonwebtoken');
+    const token = jwt.sign({ id: admin._id }, process.env.SECRET);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token: token }),
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  } else {
+    return {
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  }
+  } catch(error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  }
+}
+
+module.exports.adminGetProfiles = async (event, context) => {
+  try {
+    await connectToDatabase()
+
+    const jwt  = require('jsonwebtoken');
+    var legit = jwt.verify(event.queryStringParameters.token, process.env.SECRET);
+
+    const profiles = await Profile.find()
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ profiles }),
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  } catch(error) {
+    if (error.name === 'JsonWebTokenError'){
+      return {
+        statusCode: 401,
+        headers: {
+          "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+        },
+      }
+    }
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  }
+}
+
+module.exports.createAdminUser = async (event, context) => {
+  try {
+    await connectToDatabase()
+
+    const user = JSON.parse(event.body)
+    const crypto = require('crypto');
+    const hash = crypto.createHmac('sha256', process.env.SECRET)
+                     .update(user.password)
+                     .digest('hex');
+    const newAdmin = await AdminLogin.create({login: user.login, password: hash})
+    await newAdmin.save()
+
+    return {
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  } catch(error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.CORS_ORIGIN,
+      },
+    }
+  }
+
 }
